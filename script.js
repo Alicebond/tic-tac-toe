fd27df4b713460986e8536160d7f6fa601a19baa
Zhibin <zzbsdh000@outlook.com>
2021-06-21 14:17:34 -0230

    Add basic UI

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const Gameboard = {
-  gameBoard: [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ],
-};
+"use strict";
+
+const gameBoard = (function () {
+  const gameBoard = [
+    ["x", "x", "o"],
+    ["o", "x", "o"],
+    ["o", "o", "x"],
+  ];
+  const showBoard = function () {
+    let board = document.getElementById("gameBoard");
+
+    for (let i = 0; i < board.children.length; i++) {
+      for (let j = 0; j < board.children[i].children.length; j++) {
+        board.children[i].children[j].textContent = gameBoard[i][j];
+      }
+    }
+  };
+  return { showBoard };
+})();
+
+gameBoard.showBoard();
