89793ea37f1d8cc56eec2e7c9271959312772d14
Zhibin <zzbsdh000@outlook.com>
2021-06-28 15:00:29 -0230

    Add function to show player and computer marks

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 const gameBoard = (function () {
   const gameBoard = [
-    ["x", "x", "o"],
     ["o", "x", "o"],
+    ["x", "x", "o"],
     ["o", "o", "x"],
   ];
   const showBoard = function () {
@@ -18,4 +18,32 @@ const gameBoard = (function () {
   return { showBoard };
 })();
 
-gameBoard.showBoard();
+// gameBoard.showBoard();
+
+const player = (function () {
+  const board = document.getElementById("gameBoard");
+
+  board.addEventListener("click", function (e) {
+    if (!e.target.textContent) {
+      e.target.textContent = "X";
+      computer();
+    }
+  });
+
+  function computer() {
+    const block = document.querySelectorAll(".block");
+
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    for (let i = 0; i < block.length; i++) {
+      if (block[i].textContent) {
+        arr.splice(arr.indexOf(i), 1);
+      }
+    }
+
+    if (arr.length) {
+      let index = arr[Math.floor(Math.random() * (arr.length - 1))];
+      block[index].textContent = "O";
+      console.log(arr, index);
+    }
+  }
+})();
